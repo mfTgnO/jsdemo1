8086ff6ce5ed87f4998f674cb6f1a7656c05f008
xj2018 <tiemj@outlook.com>
2018-03-09 09:59:57 +0800

    add local jquery file.

---
+++ mjfp/chapter01/shuffle.js
@@ -0,0 +1,11 @@
+const shuffle = arr => {
+	const len = arr.length;
+	for(let i = 0; i < len; i++) {
+		let r = Math.floor(Math.random() * (len - i));
+		[arr[i], arr[i + r]] = [arr[i + r], arr[i]];
+	}
+	return arr;
+}
+
+var xxx = [11, 22, 33, 44, 55, 66, 77, 88];
+console.log(shuffle(xxx));
