c31261a4e4a3e08772a59ddb8885cb82d3ed80d2
wl <wl@google.com>
2018-03-28 19:18:28 +0800

    message

--- MasteringJavaScriptFunctionalProgramming/chapter7ProducingFunctions-Higher-OrderFunctions/Alteringfunctions/Doing things once, revisited/once.js
+++ MasteringJavaScriptFunctionalProgramming/chapter7ProducingFunctions-Higher-OrderFunctions/Alteringfunctions/Doing things once, revisited/once.js
@@ -20,3 +20,35 @@ const once2 = func => {
         return result;
     }
 };
+
+//
+const onceAndAfter = (f, g) => {
+    let done = false;
+    return (...args) => {
+        if (!done) {
+            done = true;
+            return f(...args);
+        } else {
+            return g(...args);
+        }
+    }
+};
+
+//
+const onceAndAfter2 = (f, g) => {
+    let toCall = f;
+    return (...args) => {
+        let result = toCall(...args);
+        toCall = g;
+        return result;
+    }
+};
+
+const squeak = (x) => console.log(x, "squeak!");
+const creak = (x) => console.log(x, "creak!");
+const makeSecond = onceAndAfter2(squeak, creak);
+
+makeSecond("door");
+makeSecond("door");
+makeSecond("door");
+makeSecond("door");
