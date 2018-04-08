ce2e149a0bfa0a19005bcf6dc9115d1785b23c1a
wl <wl@google.com>
2018-04-08 18:48:50 +0800

    revert

---
+++ MasteringJavaScriptFunctionalProgramming/chapter7ProducingFunctions-Higher-OrderFunctions/Alteringfunctions/Inverting results/inverting.js
@@ -0,0 +1,6 @@
+const spanishComparison = (a, b) => a.localeCompare(b, "es");
+let palabras = ["ñandú", "oasis", "mano", "natural", "mítico", "musical"];
+console.log(palabras.sort(spanishComparison));
+
+const invert = fn => (...args) => -fn(...args);
+console.log(palabras.sort(invert(spanishComparison)));
