e87c09ba148ace822ce117827f957bec32ee832c
wl <wl@gmail.com>
2018-05-12 17:58:38 +0800

    curryinging

---
+++ MasteringJavaScriptFunctionalProgramming/chapter7TransformingFunctions-CurryingandPartialApplication/1_ABitOfTheory/page378.js
@@ -0,0 +1,28 @@
+// three parameters
+const make3 = (a, b, c) => String(100 * a + 10 * b + c);
+console.log(make3(1, 2, 3));
+
+// have a sequence of functions,each with a single parameter:
+const make3curried = a => b => c => String(100 * a + 10 * b + c);
+console.log(make3curried(2)(3)(4));
+
+const make3curried2 = function (a) {
+    return function (b) {
+        return function (c) {
+            return String(100 * a + 10 * b + c);
+        };
+    };
+};
+
+console.log(make3curried2(3)(4)(5));
+
+// Suppose you had a function that calculated the VAT (Value Added Tax) for an amount:
+const addVAT = (rate, amount) => amount * (1 + rate / 100);
+console.log(addVAT(20, 500));
+console.log(addVAT(15, 200));
+
+//
+const addVATcurried = rate => amount => amount * (1 + rate / 100);
+const addNationVAT = addVATcurried(6);
+console.log(addNationVAT(1500));
+console.log(addNationVAT(2000));
