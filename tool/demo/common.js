bcfd55a6c371a224893843dbf86ab8e1a813ebc5
wl <wl@gmail.com>
2018-05-21 18:22:36 +0800

    delete

--- tool/demo/common.js
+++ tool/demo/common.js
@@ -1,1 +1,2 @@
 console.log("-------------------------------------------------- another example");
+console.log();
--- tool/demo/theGoodParts/chapter3Objects/page57.js
+++ tool/demo/theGoodParts/chapter3Objects/page57.js
@@ -1,6 +1,8 @@
+/*
 if (typeof Object.beget !== 'function') {
     Object.beget = function (o) {
-        var F = function () {};
+        var F = function () {
+        };
         F.prototype = o;
         return new F();
     };
@@ -10,3 +12,33 @@ var another_stooge = Object.beget(stooge);
 another_stooge['first-name'] = 'Harry';
 another_stooge['middle-name'] = 'Moses';
 another_stooge.nickname = 'Moe';
+*/
+
+// 3.6. Reflection
+console.log("-------------------------------------------------- another example");
+var flight = {
+    airline: "Oceanic",
+    number: 815,
+    departure: {
+        IATA: "SYD",
+        time: "2004-09-22 14:55",
+        city: "Sydney"
+    },
+    arrival: {
+        IATA: "LAX",
+        time: "2004-09-23 10:42",
+        city: "Los Angeles"
+    }
+
+};
+
+console.log("typeof flight.number:" + typeof flight.number);
+console.log("typeof flight.status:" + typeof flight.status);
+console.log("typeof flight.arrival:" + typeof flight.arrival);
+console.log("typeof flight.manifest:" + typeof flight.manifest);
+
+console.log("typeof flight.toString:" + typeof flight.toString); // function
+console.log("typeof flight.constructor:" + typeof flight.constructor);// function
+
+console.log(flight.hasOwnProperty("number"));// true
+console.log(flight.hasOwnProperty("constructor"));// false
